c1aeb6c0b9706eebe95f8b003295aa2a8e274e87
Santiago <santo1996.29@gmail.com>
2022-05-02 21:11:09 -0400

    quiz-game part 2

--- quiz-game/main.go
+++ quiz-game/main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"context"
 	"encoding/csv"
 	"flag"
 	"fmt"
 	"log"
 	"os"
 	"strings"
+	"time"
 	"unicode"
 )
 
@@ -14,26 +16,64 @@ var file = flag.String("f", "problems.csv", "CSV file to parse for questions")
 var timeout = flag.Int("t", 30, "Timeout for the quiz")
 
 func main() {
+	flag.Parse()
 	f, err := os.Open(*file)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer f.Close()
+
 	csvReader := csv.NewReader(f)
 	records, err := csvReader.ReadAll()
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	fmt.Print("Press ENTER to start the quiz")
+	_, err = fmt.Scanln()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	timeout := time.After(time.Duration(*timeout) * time.Second)
+	onePointListener := make(chan struct{})
+	quizDoneListener := make(chan struct{})
+	ctx, cancel := context.WithCancel(context.Background())
 	var points int
-	for _, record := range records {
-		var answer string
-		printQuestion(record[0])
-		requestAnswer(&answer)
-		if compareAnswers(answer, record[1]) {
+	go doQuiz(ctx, onePointListener, quizDoneListener, records)
+
+loop:
+	for {
+		select {
+		case <-timeout:
+			fmt.Println("\nTIMEOUT!")
+			cancel()
+			break loop
+		case <-onePointListener:
 			points++
+		case <-quizDoneListener:
+			break loop
 		}
 	}
+
 	fmt.Printf("RESULT: %d/%d\n", points, len(records))
+}
+
+func doQuiz(ctx context.Context, onePointListener chan<- struct{}, quizDoneListener chan<- struct{}, records [][]string) {
+	for _, record := range records {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			var answer string
+			printQuestion(record[0])
+			requestAnswer(&answer)
+			if compareAnswers(answer, record[1]) {
+				onePointListener <- struct{}{}
+			}
+		}
+	}
+	quizDoneListener <- struct{}{}
 }
 
 func prepareString(s string) string {
